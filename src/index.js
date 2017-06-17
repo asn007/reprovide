9ff91c59c38f6b13dea9c8f2996f03f1c9af18b5
Andrey Sinitsyn <andrey.sin98@gmail.com>
2017-06-17 15:56:06 +0300

    Fixed missing import error

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import consumeProviders, {
   optionalProviders
 } from "./consumeProviders";
 export { default as Provider } from "./Provider";
-export { default as ProviderComponent } from "./ProviderComponent.jsx";
+export { default as ProviderComponent } from "./ProviderComponent";
 export { default as Inject } from "./Inject";
